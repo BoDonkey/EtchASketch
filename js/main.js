5aacc28bdabfd52c6bb30b283c3446313f8e9312
Robert Means <robert.means1969@gmail.com>
2015-08-16 06:39:26 -0400

    The main body

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
 $(document).ready(function(){
-var table = $('<table></table>').addClass('foo');
-for(i=0; i<3; i++){
-    var row = $('<tr></tr>').addClass('bar').text('result ' + i);
-    table.append(row);
+makeTable(16);
+$('div').mouseleave(function(){
+	$(this).css('background-color','blue');
+})
+});
+
+
+function makeTable(tabSize){
+var elWidth = 1000 / tabSize;
+var table = $('<table></table>');
+$('#here_table').append(table);
+var rowBeg = '<tr>';
+var rowEnd = '</tr>';
+var cellCont = '<td><div id="jqCreate"></div></td>';
+
+for (i=0; i<tabSize; i++){
+	var stringHolder = '';
+	stringHolder +=rowBeg;
+	for (j=0; j<tabSize; j++){
+    	stringHolder +=cellCont;
+    }
+    stringHolder += rowEnd;
+    $('table').append(stringHolder);
+
+}
+}
+function eraseTable(){
+	$('#here_table').html('');
 }
 
-$('#here_table').append(table);
+
+
+function funcA(){
+	var sizeGrid = window.prompt('Please enter your new grid size','16');
+	console.log('done');
+	eraseTable();
+	makeTable(sizeGrid);
+}
